5dd751ea6721b2971c36295af879cc3210f8ed95
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-10-31 12:39:07 -0700

    Getting virtualized infrastructure into place
    Ensuring we tracking a lot of timing/performance metrics as things go
    on.
    
    Next step would be to create a virtualization manager class that would
    hook up to scroll events and probably actually power the rendering...

--- apps/docs/app/big-boi/BigBoiDiff.tsx
+++ apps/docs/app/big-boi/BigBoiDiff.tsx
@@ -2,13 +2,25 @@
 
 import { cn } from '@/lib/utils';
 import {
+  FileDiff,
   type FileDiffMetadata,
   parsePatchFiles,
 } from '@pierre/precision-diffs';
 import { useStableCallback } from '@pierre/precision-diffs/react';
-import { type ReactNode, type SyntheticEvent, useState } from 'react';
+import {
+  type ReactNode,
+  type SyntheticEvent,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 
-import './big-boi.module.css';
+import styles from './big-boi.module.css';
+
+interface VirtualizedFileMetadata extends FileDiffMetadata {
+  unifiedTop: number;
+  splitTop: number;
+}
 
 interface SubmitButtonProps {
   disabled?: boolean;
@@ -32,18 +44,31 @@ function SubmitButton({ children, disabled = false }: SubmitButtonProps) {
   );
 }
 
+interface VirtualizedDiffState {
+  key: string;
+  diffs: VirtualizedFileMetadata[];
+  totalHeightUnified: number;
+  totalHeightSplit: number;
+}
+
+const LINE_HEIGHT = 20;
+const LINE_HEADER_HEIGHT = 44;
+const HUNK_SEPARATOR_HEIGHT = 30;
+const FILE_GAP = 8;
+const FILE_BOTTOM_PADDING = 8;
+
+const ENABLE_RENDERING = true;
+
 export function BigBoiDiff() {
   const [fetching, setFetching] = useState(false);
+  // The BIG BOI
   const [url, setURL] = useState('https://github.com/nodejs/node/pull/59805');
-  const [_data, setDiff] = useState<{
-    key: string;
-    diffs: FileDiffMetadata[];
-  } | null>(null);
+  const ref = useRef<HTMLDivElement>(null);
+  const [data, setDiff] = useState<VirtualizedDiffState | null>(null);
   const handleSubmit = useStableCallback(
     async (event: SyntheticEvent<HTMLFormElement>) => {
       event.preventDefault();
       const parsedURL = new URL(url);
-      console.log('ZZZZZ - fetching url', parsedURL);
       if (parsedURL.hostname !== 'github.com') {
         return;
       }
@@ -55,16 +80,17 @@ export function BigBoiDiff() {
         !/^\d+(\.patch)?$/.test(finalSegment) ||
         pullSegment !== 'pull'
       ) {
-        console.error('ZZZZ - invalid url');
+        console.error('Invalid URL', parsedURL);
         return;
       }
-      console.log('Valid url', url);
       setFetching(true);
 
       try {
+        console.time('--     request time');
         const response = await fetch(
           `/api/fetch-pr-patch?path=${encodeURIComponent(parsedURL.pathname)}`
         );
+        console.timeEnd('--     request time');
 
         if (!response.ok) {
           const error = await response.json();
@@ -72,25 +98,106 @@ export function BigBoiDiff() {
           return;
         }
 
+        console.time('--     parsing json');
         const data = await response.json();
-        console.time('parsing time');
+        console.timeEnd('--     parsing json');
+
+        console.time('--  parsing patches');
         const parsedPatches = parsePatchFiles(data.content);
-        console.timeEnd('parsing time');
-        const diffs: FileDiffMetadata[] = [];
+        console.timeEnd('--  parsing patches');
+        const diffs: VirtualizedFileMetadata[] = [];
+        console.time('-- computing layout');
+        let totalHeightUnified = 0;
+        let totalHeightSplit = 0;
+
+        // Hacks to make the big boi smaller...
+        // parsedPatches.length = 1;
+        // parsedPatches[0].files.length = 40;
+        // console.log('ZZZZ - before trim', parsedPatches[0].files.length);
+        // const file = parsedPatches[0].files[7];
+        // parsedPatches[0].files.length = 0;
+        // parsedPatches[0].files.push(file);
+
         for (const patch of parsedPatches) {
           for (const file of patch.files) {
-            diffs.push(file);
+            if (diffs.length > 0) {
+              totalHeightUnified += FILE_GAP;
+              totalHeightSplit += FILE_GAP;
+            }
+            // It would be about twice as fast to just straight ts-ignore and
+            // manipulate this
+            diffs.push({
+              ...file,
+              unifiedTop: totalHeightUnified,
+              splitTop: totalHeightSplit,
+            });
+            totalHeightUnified += LINE_HEADER_HEIGHT;
+            totalHeightSplit += LINE_HEADER_HEIGHT;
+            totalHeightUnified += file.unifiedLineCount * LINE_HEIGHT;
+            totalHeightSplit += file.splitLineCount * LINE_HEIGHT;
+            const hunkCount = file.hunks.length;
+            const [firstHunk] = file.hunks;
+            if (firstHunk != null) {
+              if (firstHunk.additionStart > 1 || firstHunk.deletedStart > 1) {
+                let hunkSize =
+                  (HUNK_SEPARATOR_HEIGHT + FILE_GAP * 2) * (hunkCount - 1);
+                hunkSize += HUNK_SEPARATOR_HEIGHT + FILE_GAP;
+                totalHeightSplit += hunkSize;
+                totalHeightUnified += hunkSize;
+              } else {
+                const hunkSize =
+                  (HUNK_SEPARATOR_HEIGHT + FILE_GAP * 2) * (hunkCount - 1);
+                totalHeightSplit += hunkSize;
+                totalHeightUnified += hunkSize;
+              }
+            }
+            if (hunkCount > 0) {
+              totalHeightUnified += FILE_BOTTOM_PADDING;
+              totalHeightSplit += FILE_BOTTOM_PADDING;
+            }
           }
         }
-        setDiff({ key: parsedURL.toString(), diffs });
+        console.timeEnd('-- computing layout');
+        const diffData: VirtualizedDiffState = {
+          key: parsedURL.toString(),
+          diffs,
+          totalHeightSplit,
+          totalHeightUnified,
+        };
+        console.log(diffData);
+        setDiff(diffData);
         // TODO: Parse the patch content and set the diffs state
         // setDiff(parsedPatches);
       } catch (error) {
-        console.error('Error fetching patch:', error);
+        console.error('Error fetching or processing patch:', error);
       }
       setFetching(false);
     }
   );
+  useLayoutEffect(() => {
+    const { current: containerWrapper } = ref;
+    if (!ENABLE_RENDERING || data == null || containerWrapper == null) {
+      return;
+    }
+    containerWrapper.style.height = `${data.totalHeightSplit + FILE_GAP}px`;
+    void (async () => {
+      const instances: Promise<unknown>[] = [];
+      for (const fileDiff of data.diffs) {
+        const instance = new FileDiff(
+          {
+            theme: 'pierre-dark',
+            // diffStyle: 'unified',
+          },
+          true
+        );
+        const fileContainer = document.createElement('file-diff');
+        fileContainer.style.top = `${fileDiff.splitTop}px`;
+        containerWrapper.appendChild(fileContainer);
+        instances.push(instance.render({ fileDiff, fileContainer }));
+      }
+      await Promise.all(instances);
+    })();
+  }, [data]);
   return (
     <>
       <div className="relative mx-auto w-5xl max-w-full px-5">
@@ -108,6 +215,7 @@ export function BigBoiDiff() {
         </form>
         <p className="text-sm">The bigger the better ;)</p>
       </div>
+      <div ref={ref} className={styles.wrapper} />
     </>
   );
 }
